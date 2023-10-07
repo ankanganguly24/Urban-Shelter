75e0e5dc790a9458a6681776758eff7dfd1921f1
Ankan <ankanganguly24@gmail.com>
2023-10-07 14:09:46 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import React from 'react'
+import { BrowserRouter , Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
 const App = () => {
   return (
-    <div className="font-extrabold bg-zinc-200">App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/Profile' element={<Profile/>}/>
+      <Route path='/About' element={<About/>}/>
+      <Route path='/SignIn' element={<SignIn/>}/>
+      <Route path='/SignUp' element={<SignUp/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
