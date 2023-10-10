65d262e818c7f4d51a18965ec9115f551e8c78ea
Ankan <ankanganguly24@gmail.com>
2023-10-10 17:30:14 +0530

    Bakend setup and connected.

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongo");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
